09dcbc4f0148fa37cdcc48abb359f38aaf9f4a73
beeforbacon <51864803+beeforbacon@users.noreply.github.com>
2022-05-03 10:52:20 -0500

    Update EmailContentRenderer.cs
    Just a few semantics changes and additions to the email. Please feel free to keep some changes but toss others.

--- src/Reacher.Common/Logic/EmailContentRenderer.cs
+++ src/Reacher.Common/Logic/EmailContentRenderer.cs
@@ -25,15 +25,15 @@ public class EmailContentRenderer : IEmailContentRenderer
             MarkdownContentItems = new[] {
         $@"Hey there,
 
-We just wanted to let you know that your tip of **{cost:c}** was received, and Reacher has delivered your email to **{reacherName}**."
+We just wanted to let you know that your payment of **{cost:c}** was received, and Reacher has delivered your email to **{reacherName}**."
             },
             OtherEmail = new() { Html = html, Subject = subject, Label = "**Your Email:**", AttachmentCount = attachmentCount },
             MarkdownContentItemsAfter = new[] {
-                @$"Please note that **all tips go directly to {reacherName}**. 
+                @$"Please note that **all payments go directly to {reacherName}**. 
  
 A direct response to this email will be *ignored*."
             },
-            BottomNote = $"We are sending you this email because you sent an email to **{reacherEmail}**, and {reacherName} has chosen to use Reacher",
+            BottomNote = $"We are sending you this email because you sent an email to **{reacherEmail}**, and {reacherName} has chosen to use Reacher to prevent spam.",
             Invite = true
         };
         return _templateService.GetTemplateHtmlAsStringAsync("Main", new RazorReacherEmail(email));
@@ -43,28 +43,30 @@ A direct response to this email will be *ignored*."
     {
         var email = new ReacherEmail
         {
-            Summary = $@"Reacher has received your email. Please provide a tip to get your email delivered.",
+            Summary = $@"Reacher has received your email. Please pay the spam prevention invoice to get your email delivered.",
             MarkdownContentItems = new[] {
         $@"Hey there,
 
 We see you wanted to reach **{reacherName}**, but your email hasn't been delivered yet.
 
-**{reacherName}** uses Reacher to know which messages are worth reading.
+**{reacherName}** uses Reacher to prevent spam from entering their inbox.
 
-All you gotta do is add a **{cost:c}** tip, then Reacher will deliver your email below."
+All you gotta do is add a **{cost:c}** Lightning Network payment to prove you are not sending spam. Reacher will then deliver your email below.
+
+If you do not have a way to pay a Lightning Network invoice, we recommend the Strike app. For more information click here: <a href="https://strike.me">Strike</a>"
             },
             OtherEmail = new() { Html = html, Subject = subject, Label = "**Your Email:**", AttachmentCount = attachmentCount },
             Actions = new List<EmailAction> {
                 new(){ActionName = "Add a Tip Now", Url = "https://www.reacher.me/tip/" + emailId}
             },
             MarkdownContentItemsAfter = new[] {
-                @$"Please note that **all tips go directly to {reacherName}**. 
+                @$"Please note that **all payments go directly to {reacherName}**. 
   
 {reacherName} is constantly bombarded by emails, so if you want to get their attention, add a tip! 
   
 Also note that a direct response to this email will be *ignored*."
             },
-            BottomNote = $"We are sending you this email because you sent an email to **{reacherEmail}**, and {reacherName} has chosen to use Reacher",
+            BottomNote = $"We are sending you this email because you sent an email to **{reacherEmail}**, and {reacherName} has chosen to use Reacher to prevent spam.",
             Invite = true
         };
         return _templateService.GetTemplateHtmlAsStringAsync("Main", new RazorReacherEmail(email));// _contentProvider.RunTemplate(email);
